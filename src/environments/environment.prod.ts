da2cd291d4664ac91a31013d2cb5b36454078631
Enno Rosinger <enno@rosinger.net>
2022-11-13 15:54:09 +0100

    removed a secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  contentful: {
-    space: '5z21gx3h7sb2',
-    environment: "master",
-    accessToken: 'ecQ2l4UMTYi1MU3Szr2iNLep8Ak_pNp5i3Nxe1qU3VA',
-  },
-  mailService:{
-    url: 'https://open-letter-mailer.herokuapp.com'
-  }
+  production: true
 };
