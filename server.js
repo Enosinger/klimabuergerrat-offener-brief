0a3105865394abe38680ad7e6c120bb6052a79b8
Enno Rosinger <enno@rosinger.net>
2025-03-18 15:18:51 +0100

    adjust server again with path resolve

--- server.js
+++ server.js
@@ -2,33 +2,28 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-//  Define funtion that redirects to https if necessary
+// Force HTTPS on Heroku
 function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (
-    !req.secure &&
-    req.get("x-forwarded-proto") !== "https" &&
-    process.env.NODE_ENV !== "development"
-  ) {
+  if (!req.secure && req.get("x-forwarded-proto") !== "https" && process.env.NODE_ENV !== "development") {
     return res.redirect("https://" + req.get("host") + req.url);
   }
   next();
 }
-// Use the function on the express app
 app.use(requireHTTPS);
 
-app.use(
-  express.static(path.join(__dirname, "dist", "klimabuergerrat-offener-brief"))
-);
+// Serve static files
+const distPath = path.resolve("dist", "klimabuergerrat-offener-brief");
+console.log("Serving static files from:", distPath);
+app.use(express.static(distPath));
 
+// Serve index.html
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "dist", "klimabuergerrat-offener-brief", "index.html")
-  );
+  const indexPath = path.join(distPath, "index.html");
+  console.log("Serving index.html from:", indexPath);
+  res.sendFile(indexPath);
 });
 
-console.log("Serving static files from:", path.join(__dirname, "dist", "klimabuergerrat-offener-brief"));
-console.log("Serving index.html from:", path.join(__dirname, "dist", "klimabuergerrat-offener-brief", "index.html"));
-
-// Serve the express app on the port configured in the env variables - or on 8080 if not specified
-app.listen(process.env.PORT || 8080);
+// Start server
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Server started on port", process.env.PORT || 8080);
+});
