d12ea53bcb07fcc04a17a8e78e859d34aa0fca92
Enno Rosinger <enno@rosinger.net>
2025-03-18 15:35:49 +0100

    another try with process.cwd()

--- server.js
+++ server.js
@@ -4,22 +4,28 @@ const app = express();
 
 // Force HTTPS on Heroku
 function requireHTTPS(req, res, next) {
-  if (!req.secure && req.get("x-forwarded-proto") !== "https" && process.env.NODE_ENV !== "development") {
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
     return res.redirect("https://" + req.get("host") + req.url);
   }
   next();
 }
 app.use(requireHTTPS);
 
-// Serve static files
-const distPath = path.resolve("dist", "klimabuergerrat-offener-brief");
-console.log("Serving static files from:", distPath);
+const distPath = path.join(
+  process.cwd(),
+  "dist",
+  "klimabuergerrat-offener-brief"
+);
+console.log("Resolved dist path:", distPath);
 app.use(express.static(distPath));
 
-// Serve index.html
 app.get("/*", function (req, res) {
   const indexPath = path.join(distPath, "index.html");
-  console.log("Serving index.html from:", indexPath);
+  console.log("Resolved index.html path:", indexPath);
   res.sendFile(indexPath);
 });
 
