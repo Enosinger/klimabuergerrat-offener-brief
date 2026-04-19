f78cc3c93aebac8a6705bede9f266b578b2927fd
Enno Rosinger <enno@rosinger.net>
2026-04-19 14:54:55 +0200

    solve error in tsconfig.json

--- src/app/sign-letter-modal/sign-letter-modal.component.ts
+++ src/app/sign-letter-modal/sign-letter-modal.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Component({
     selector: 'app-sign-letter-modal',
